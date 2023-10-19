ef1d80a3510f229f9d35f0a19dc31cea02333b31
Maciej Jurczak <mac.jurczak@o2.pl>
2023-10-19 17:45:09 +0200

    Finishing orders-service getMapping methods

--- orders/src/main/java/com/mac2work/orders/repository/OrderRepository.java
+++ orders/src/main/java/com/mac2work/orders/repository/OrderRepository.java
@@ -1,8 +1,10 @@
 package com.mac2work.orders.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.mac2work.orders.model.Order;
@@ -12,6 +14,12 @@ public interface OrderRepository extends JpaRepository<Order, Long>{
 
 	List<Order> findAllByUserId(Long userId);
 
-	Order getByUserId(Long userId);
+	Order findByUserId(Long userId);
+
+	@Query("select o from orders o where o.userId = ?1 and o.to < ?2")
+	List<Order> findAllArchivalByUserId(Long userId, LocalDate currentTime);
+
+	@Query("select o from orders o where o.id = ?1 and o.userId = ?2 and  o.to < ?3")
+	Order findArchivalByUserId(Long id, Long userId, LocalDate now);
 
 }
