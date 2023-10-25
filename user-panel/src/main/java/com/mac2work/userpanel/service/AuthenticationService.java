a0da5af8665426b58417ec787acc4d1a1a1b385d
Maciej Jurczak <mac.jurczak@o2.pl>
2023-10-25 18:34:27 +0200

    Implementing Jwt authentication WIP

--- user-panel/src/main/java/com/mac2work/userpanel/service/AuthenticationService.java
+++ user-panel/src/main/java/com/mac2work/userpanel/service/AuthenticationService.java
@@ -1,5 +1,22 @@
 package com.mac2work.userpanel.service;
 
-public class AuthenticationService {
+import org.springframework.stereotype.Service;
 
+import com.mac2work.userpanel.response.AuthenticationResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+	
+	public AuthenticationResponse register() {
+		return null;
+	}
+
+	public AuthenticationResponse authenticate() {
+		return null;
+	}
+
+	
 }
--- user-panel/src/main/java/com/mac2work/userpanel/service/JwtService.java
+++ user-panel/src/main/java/com/mac2work/userpanel/service/JwtService.java
@@ -1,5 +1,79 @@
 package com.mac2work.userpanel.service;
 
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service
 public class JwtService {
 
+	private static final String SECRET_KEY = "70489BD183A6BE5C2F1090C85B4C05B1601F27E1F2D5AA14B92B2156BE585C20";
+
+	public String extractUsername(String token) {
+		
+		return extractClaim(token, Claims::getSubject);
+	}
+	
+	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = extractAllClaims(token);
+		return claimsResolver.apply(claims);
+	}
+	public String generateToken(UserDetails userDetails) {
+		return generateToken(new HashMap<>(), userDetails);
+	}
+	
+	public String generateToken(
+			Map<String, Object> extraClaims,
+			UserDetails userDetails
+	) {
+		return Jwts
+				.builder()
+				.setClaims(extraClaims)
+				.setSubject(userDetails.getUsername())
+				.setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60  * 60 * 12))
+				.signWith(getSigningKey(), SignatureAlgorithm.HS256)
+				.compact();
+		
+	}
+	
+	public boolean isTokenValid(String token, UserDetails userDetails) {
+		final String username = extractUsername(token);
+		return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+	}
+	
+	private boolean isTokenExpired(String token) {
+		return extractExpiration(token).before(new Date());
+	}
+
+	private Date extractExpiration(String token) {
+		return extractClaim(token, Claims::getExpiration);
+	}
+
+	private Claims extractAllClaims(String token) {
+		
+		return Jwts
+				.parserBuilder()
+				.setSigningKey(getSigningKey())
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
+				
+	}
+
+	private Key getSigningKey() {
+		byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
 }
